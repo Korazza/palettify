fdbd2f3d65e3f2ee473d4d065051ee1b3ff91d36
Korazza <corattiroberto@gmail.com>
2022-11-09 00:22:15 +0100

    refactor(color.py): convert to dataclass and add rgb parameter

--- color.py
+++ color.py
@@ -1,10 +1,24 @@
+from dataclasses import dataclass
+import numpy as np
+
 from utils import hex_to_rgb
 
 
+@dataclass
 class Color:
-    def __init__(self, hex: str):
-        self.hex = hex
-        self.rgb = hex_to_rgb(hex)
+    """Class for storing a color"""
+
+    hex: str = None
+    rgb: np.ndarray[int] = None
+
+    def __post_init__(self):
+        if self.hex == None and self.rgb == None:
+            self.hex = "#000000"
+            self.rgb = np.array([0, 0, 0])
+        elif self.hex == None:
+            self.hex = f"#{self.rgb[0]:x}{self.rgb[1]:x}{self.rgb[2]:x}"
+        else:
+            self.rgb = np.array(hex_to_rgb(self.hex))
 
     def __str__(self) -> str:
         return f"<Color {self.hex}>"
