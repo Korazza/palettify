13057a339ff32a656c5cb9a9fb989283346fdd7c
Korazza <corattiroberto@gmail.com>
2022-11-11 00:39:39 +0100

    chore(palettify.py): refactor

--- palettify.py
+++ palettify.py
@@ -5,10 +5,13 @@ from PIL import Image
 import numpy as np
 
 from palette import Palette
+import config
 import palettes_loader
 
 
 def palettify_image(palette: Palette, image: Image.Image) -> np.ndarray:
+    palette.load_colors()
+    palette.cache_colorcube()
     image = np.asarray(image)
     colorcube = palette.load_colorcube()
     shape = image.shape
@@ -17,18 +20,17 @@ def palettify_image(palette: Palette, image: Image.Image) -> np.ndarray:
     return output_image.reshape(shape[0], shape[1], 3).astype(np.uint8)
 
 
-def main():
-    args = sys.argv
-    if len(args) == 1:
-        print(f"Usage: {args[0]} <image path>")
+def main(args: list[str], palettes_path: str = config.PALETTES_DIR):
+    if len(args) <= 0 or len(args) >= 2:
+        print("Usage: palettify.py <image path>")
         exit(1)
 
-    image_path = args[1]
+    image_path = args[0]
     if not os.path.exists(image_path):
         print(f'Image "{image_path}" does not exist')
         exit(1)
 
-    palettes = palettes_loader.load_palettes()
+    palettes = palettes_loader.load_palettes(palettes_path)
 
     print("Choose a palette")
     for i, palette in enumerate(palettes):
@@ -46,11 +48,11 @@ def main():
     image = Image.open(image_path)
 
     image_result = Image.fromarray(palettify_image(palette, image))
-
     image_result.save(f"output.png")
 
     palette.log(f"Done in {(time.time() - start):.3f}s")
+    exit(0)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__":  # pragma: no cover
+    main(sys.argv[1:])
