a051d5905b77b82981b0558983bc41056d48807c
Korazza <corattiroberto@gmail.com>
2022-11-11 00:39:22 +0100

    chore(palettes_loader.py): refactor

--- palettes_loader.py
+++ palettes_loader.py
@@ -1,18 +1,19 @@
 import os
-from config import PALETTES_DIR, COLORS_EXTENSION
+
+import config
 from palette import Palette
 
 
-def load_palettes(path: str = PALETTES_DIR) -> list[Palette]:
+def load_palettes(path: str = config.PALETTES_DIR) -> list[Palette]:
     palettes: list[Palette] = []
     for palette_file in os.listdir(path):
         palette_path = os.path.join(path, palette_file)
         isdir = os.path.isdir(palette_path)
-        if not palette_file.endswith(COLORS_EXTENSION) and not isdir:
+        if not palette_file.endswith(config.COLORS_EXTENSION) and not isdir:
             continue
         if isdir:
             for variant in os.listdir(palette_path):
-                if not variant.endswith(COLORS_EXTENSION):
+                if not variant.endswith(config.COLORS_EXTENSION):
                     continue
                 palettes.append(Palette(os.path.join(palette_path, variant)))
         else:
